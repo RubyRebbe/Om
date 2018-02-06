39298544591325f82a5d99e7b3bebfff91cc9a7f
Tsvi Bar-David <tsvi.bardavid@gmail.com>
2018-02-06 14:58:43 -0800

    eliminated udo

---
+++ spec/factory_spec.rb
@@ -0,0 +1,17 @@
+describe "Factory" do
+  it "can construct a factory for instance objects out of a Frame" do
+    skip
+  end
+
+	it "each instance object is a Frame"
+
+	it "all the instance objects share the same public interface of messages"
+
+	it "each instance object has the same data structure for representing state"
+
+	it "each instance object has the same initial state"
+
+	it "has a method (e.g. create) for creating instance objects"
+
+	it "is there a factory for factories?"
+end
